db3e2ed8d3790a5f50b9939244512972f827d256
Jesse House <mail@jessehouse.com>
2019-05-22 18:03:36 -0700

    add 'snap center'

--- misc/window_snap.py
+++ misc/window_snap.py
@@ -122,6 +122,7 @@ ctx.keymap(
         "snap bottom left": grid(1, 2, 2, 2),
         "snap bottom right": grid(2, 2, 2, 2),
         "snap (screen | window)": grid(1, 1, 1, 1),
+        "snap center": grid(2, 2, 8, 8, 6, 6),
         "snap next": next_screen,
         "snap last": previous_screen,
         "window next screen": next_screen,
